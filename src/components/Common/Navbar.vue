54e74f1d0ac9dcedd1817f0fa9fc0f66cf93df93
stathismor <emoraitidis@gmail.com>
2022-01-07 18:26:20 +0000

    Change navbar title

--- src/components/Common/Navbar.vue
+++ src/components/Common/Navbar.vue
@@ -8,7 +8,7 @@
         class="d-inline-block align-top"
         alt=""
       />
-      Anarres
+      Anarres.fm
     </a>
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
