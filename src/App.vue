96113aaac103b61ddeb6c55760130356258a282d
stathismor <emoraitidis@gmail.com>
2022-02-26 16:05:06 +0000

    Test shit

--- src/App.vue
+++ src/App.vue
@@ -21,34 +21,30 @@ export default {
     RadioPlayer,
   },
   methods: {
-    appHeight() {
-      document.documentElement.style.setProperty(
-        '--app-height',
-        `${window.innerHeight}px`
-      );
+    resize() {
+      // We execute the same script as before
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
     },
   },
   created() {
-    window.addEventListener('resize', this.appHeight);
-    this.appHeight();
+    // We listen to the resize event
+    window.addEventListener('resize', this.resize);
+    this.resize();
   },
   beforeDestroy() {
-    window.removeEventListener('resize', this.appHeight);
+    window.removeEventListener('resize', this.resize);
   },
 };
 </script>
 
 <style>
 @import '../assets/css/variables.css';
-
-:root {
-  height: var(--app-height);
-}
-
 body {
   margin: 0;
   min-height: 100vh;
-  height: 100%;
+  height: 100vh; /* Use vh as a fallback for browsers that do not support Custom Properties */
+  height: calc(var(--vh, 1vh) * 100);
   background-color: var(--background-color) !important;
   background: var(--background-color) url(../assets/img/background.svg)
     no-repeat center center;
